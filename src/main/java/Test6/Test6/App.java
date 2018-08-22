288f8e8b5530d630b95c2b4af5ce1219b477001b
Geeta-Shetty <40625998+Geeta-Shetty@users.noreply.github.com>
2018-08-22 15:35:35 +0530

    PullChanges

--- src/main/java/Test6/Test6/App.java
+++ src/main/java/Test6/Test6/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         
         System.out.println("Chnages made for branch");
+        System.out.println("Pull Changes");
+
     }
 }
